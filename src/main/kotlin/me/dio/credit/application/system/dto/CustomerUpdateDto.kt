d477684306b5756a588ac3d4d1ef399a5b3158d0
michelypsouza <michely.psouza@gmail.com>
2024-02-26 20:18:39 -0300

    construindo a camada controller (resource)

---
+++ src/main/kotlin/me/dio/credit/application/system/dto/CustomerUpdateDto.kt
@@ -0,0 +1,21 @@
+package me.dio.credit.application.system.dto
+
+import me.dio.credit.application.system.entity.Customer
+import java.math.BigDecimal
+
+data class CustomerUpdateDto(
+    val firstName: String,
+    val lastName: String,
+    val income: BigDecimal,
+    val zipcode: String,
+    val street: String
+) {
+    fun toEntity(customer: Customer) : Customer {
+        customer.firstName = this.firstName
+        customer.lastName = this.lastName
+        customer.income = this.income
+        customer.address.zipcode = this.zipcode
+        customer.address.street = this.street
+        return customer
+    }
+}
